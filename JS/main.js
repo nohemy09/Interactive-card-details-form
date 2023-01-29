d1eb2aa5a83ffa536a10a51e5171384cfa69d6ac
nemo2109 <114691858+nohemy09@users.noreply.github.com>
2023-01-29 11:17:54 -0600

    Funciolidad con JS iniciada

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,15 @@
 import '../assets/styles/style.scss';
 
+const cardholder = document.getElementById('cardholder');
+const cardnumber = document.getElementById('cardnumber');
+const expm = document.getElementById('expm');
+const expy = document.getElementById('expy');
+const cvc = document.getElementById('cvc');
+const error = document.getElementById('error-card');
+const number = document.getElementById('error-number');
+const expnum = document.getElementById('error-exp');
+const errorcvc = document.getElementById('error-cvc');
+const form = document.getElementById('forma');
+
 
 
